4a02d6721457f673a4debe1895085f46b11a5a82
mickmed <mick2090@gmail.com>
2020-12-06 10:40:35 -0500

    fix controllers

--- controllers/locations.js
+++ controllers/locations.js
@@ -4,18 +4,25 @@ const Op = Sequelize.Op
 
 
 
-const findAllWithImages = async () => {
+const getLocations = async (req, res) => {
     const users = await Location.findAll({
         // include: [{
         //     model: Image
         // }]
     });
     console.log("All users with their associated tasks:", JSON.stringify(users, null, 4));
+  
+    res.json(users)
 }
 
-const run = async () => {
-    await findAllWithImages()
-    await process.exit()
-}
+// const run = async () => {
+//     await findAllWithImages()
+//     await process.exit()
+// }
 
-run()
+
+
+module.exports = {
+
+    getLocations
+}
--- routes/locations.js
+++ routes/locations.js
@@ -5,9 +5,9 @@ const router = Router()
 
 
 router.get('/locations', controllers.getLocations)
-router.get('/locations/:id', controllers.getLocation)
-router.post('/locations', controllers.createLocation)
-router.put('/locations/:id', controllers.updateLocation)
-router.delete('/locations/:id', controllers.deleteLocation)
+// router.get('/locations/:id', controllers.getLocation)
+// router.post('/locations', controllers.createLocation)
+// router.put('/locations/:id', controllers.updateLocation)
+// router.delete('/locations/:id', controllers.deleteLocation)
 
 module.exports = router
